d8f83407d6bd6bd130ae736c5b470e4e607ff5c5
michal92299 <chudybyczek2137@gmail.com>
2026-08-05 21:42:32 +0200

    Create seat.rs

---
+++ src/handlers/seat.rs
@@ -0,0 +1,23 @@
+use smithay::input::pointer::CursorImageStatus;
+use smithay::input::{Seat, SeatHandler, SeatState};
+use smithay::reexports::wayland_server::protocol::wl_surface::WlSurface;
+use smithay::delegate_seat;
+
+use crate::state::HwdeState;
+
+impl SeatHandler for HwdeState {
+    type KeyboardFocus = WlSurface;
+    type PointerFocus = WlSurface;
+    type TouchFocus = WlSurface;
+
+    fn seat_state(&mut self) -> &mut SeatState<HwdeState> {
+        &mut self.seat_state
+    }
+
+    fn cursor_image(&mut self, _seat: &Seat<Self>, image: CursorImageStatus) {
+        self.cursor_status = image;
+    }
+
+    fn focus_changed(&mut self, _seat: &Seat<Self>, _focused: Option<&WlSurface>) {}
+}
+delegate_seat!(HwdeState);
